d1b6eea4826aa0c187128d5493a63bd6581c9590
syahmisajid-plai <syahmisajid@plai.ac.id>
2026-01-20 21:34:35 +0700

    ganti whisper untuk lupa kata6

--- backend/main.py
+++ backend/main.py
@@ -24,9 +24,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://fast-api-speak-v0.vercel.app",  # frontend production
+        "http://127.0.0.1:5173",
+        "https://fast-api-speak-v0.vercel.app",
+        "https://fastapi-speak-v0.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
