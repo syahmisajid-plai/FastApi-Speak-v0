46b84579334eae162e6f764d6b9d50900391afcf
syahmisajid-plai <syahmisajid@plai.ac.id>
2026-04-12 17:01:44 +0700

    try audio storage system1

--- backend/utils/audio_vocab.py
+++ backend/utils/audio_vocab.py
@@ -8,7 +8,7 @@ from google.cloud import texttospeech
 from config import client_tts  # client TTS dari config.py
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 BUCKET = "audio"
