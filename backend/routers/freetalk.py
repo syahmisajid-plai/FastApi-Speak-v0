a1a7589597c0474445ab9f630b0b2c9419cdda9b
syahmisajid-plai <syahmisajid@plai.ac.id>
2026-04-20 20:47:30 +0700

    try monitorting api_usage_logs5

--- backend/routers/freetalk.py
+++ backend/routers/freetalk.py
@@ -6,8 +6,11 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
-from db import get_messages, save_message
+from db import get_messages, save_message, insert_api_log
 from prompts.freetalk_prompt import FREE_TALK_PROMPT
+
+import time
+from utils.monitoring_cost import calculate_all_costs
 
 
 router = APIRouter(prefix="/free-talk", tags=["Free Talk"])
@@ -25,6 +28,7 @@ llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7, streaming=USE_STREAMING)
 
 
 def run_freetalk(session_id: str, user_id: str, user_message: str):
+    start_time = time.time()
     try:
         session_key = f"{session_id}_{user_id}_freetalk"
 
@@ -53,6 +57,7 @@ def run_freetalk(session_id: str, user_id: str, user_message: str):
         # 3. Generate response
         # -----------------------------
         response = llm.invoke(messages)
+        answer = response.content
 
         # -----------------------------
         # 4. Simpan ke DB
@@ -60,7 +65,44 @@ def run_freetalk(session_id: str, user_id: str, user_message: str):
         save_message(session_key, user_id, "freetalk", "user", user_message)
         save_message(session_key, user_id, "freetalk", "assistant", response.content)
 
-        return response.content
+        # =============================
+        # 5. COST TRACKING
+        # =============================
+
+        duration_ms = int((time.time() - start_time) * 1000)
+
+        costs = calculate_all_costs(
+            system_prompt=FREE_TALK_PROMPT,
+            history_messages=history_messages,
+            user_input=user_message,
+            llm_output=answer,
+            tts_characters=0
+        )
+
+        log_data = {
+            "user_id": user_id,
+            "session_id": session_key,
+            "endpoint": "/freetalk",
+            "feature": "freetalk",
+            "method": "POST",
+
+            "status_code": 200,
+            "duration_ms": duration_ms,
+
+            "tokens_input": costs["tokens_input"],
+            "tokens_output": costs["tokens_output"],
+
+            "characters": 0,
+
+            "stt_cost": costs["stt_cost"],
+            "llm_cost": costs["llm_cost"],
+            "tts_cost": costs["tts_cost"],
+            "total_cost": costs["total_cost"],
+        }
+
+        insert_api_log(log_data)
+
+        return answer
 
     except Exception as e:
         print("❌ FREETALK ERROR:", e)
