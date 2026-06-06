0637d8c1023f61090f1ef22fcbf3759609522d7d
MacBook Air <macbookair@MacBook-Air-Macbook.local>
2026-06-06 12:25:20 +0700

    try whisper in android test7

--- backend/main.py
+++ backend/main.py
@@ -112,7 +112,7 @@ app.add_middleware(
         "https://192.168.110.153:5173", # Kopken
         "https://192.168.1.26:5173",
         "https://172.16.10.178:5173", # testing local hp PLAI
-        "https://172.16.0.134:5173"
+        "https://172.16.0.134:5173",
         "https://192.168.10.201:5173", # testing local hp Tomoro
     ],
     allow_credentials=True,
